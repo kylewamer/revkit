c0fac61c6c508ae82a7f320523ec2a412cf947e6
KyleWamer <kwamer@photonic.com>
2024-12-30 15:29:10 -0800

    python linting

--- test/test_esop.py
+++ test/test_esop.py
@@ -1,25 +1,61 @@
-from revkit import truth_table, esop_from_pprm, esop_from_optimum_pkrm, exact_esop, esop_from_tt_maxsat, print_esop_as_exprs
+from revkit import (
+    truth_table,
+    cube,
+    esop_from_pprm,
+    esop_from_optimum_pkrm,
+    exact_esop,
+    esop_from_tt_maxsat_uc,
+    esop_from_tt_maxsat_rc2,
+    esop_from_tt_maxsat_linear,
+)
 import pytest
 
 _num_vars = 3
+
 
 @pytest.fixture
 def truthtable():
     return truth_table.from_binary("10010110")
 
-def test_esop_from_optimum_pkrm(truthtable):
-    esop = esop_from_optimum_pkrm(truthtable)
-    print_esop_as_exprs(esop, _num_vars)
 
-def test_esop_from_pprm(truthtable):
+@pytest.fixture
+def expected_esop():
+    return [cube("1--"), cube("-1-"), cube("--1")]
+
+
+@pytest.fixture
+def cost_function() -> int:
+    def cost_fn(_cube: cube):
+        return _cube.num_literals + 1
+
+    return cost_fn
+
+
+def test_esop_from_optimum_pkrm(truthtable, expected_esop):
+    esop = esop_from_optimum_pkrm(truthtable)
+    assert esop == expected_esop
+
+
+def test_esop_from_pprm(truthtable, expected_esop):
     esop = esop_from_pprm(truthtable)
-    print_esop_as_exprs(esop, _num_vars)
+    assert esop == expected_esop
+
 
 def test_exact_esop(truthtable):
     esops = exact_esop(truthtable)
-    for esop in esops:
-        print_esop_as_exprs(esop, _num_vars)
+    assert len(esops) == 10
 
-def test_esop_from_tt_maxsat(truthtable):
-    esop = esop_from_tt_maxsat(truthtable)
-    print_esop_as_exprs(esop, _num_vars)
+
+def test_esop_from_tt_maxsat_rc2(truthtable, expected_esop, cost_function):
+    esop = esop_from_tt_maxsat_rc2(truthtable, cost_function)
+    assert esop == expected_esop
+
+
+def test_esop_from_tt_maxsat_linear(truthtable, cost_function):
+    esop = esop_from_tt_maxsat_linear(truthtable, cost_function)
+    assert esop == [cube("1--"), cube("-10"), cube("-01")]
+
+
+def test_esop_from_tt_maxsat_uc(truthtable, cost_function):
+    esop = esop_from_tt_maxsat_uc(truthtable, cost_function)
+    assert esop == [cube("--0"), cube("-0-"), cube("1--")]
